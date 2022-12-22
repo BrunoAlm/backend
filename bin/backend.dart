b829756d2fc2ae1db129c2f3373113f651b959d6
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-12-21 21:50:20 -0300

    att

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,21 +1,15 @@
 import 'package:shelf/shelf.dart';
 
-import 'api/blog_api.dart';
-import 'api/login_api.dart';
+import 'apis/blog_api.dart';
+import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
 import 'utils/custom_env.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
-  
-  var cascadeHandler = Cascade()
-      .add(
-        LoginApi().handler,
-      )
-      .add(
-        BlogApi().handler,
-      )
-      .handler;
+
+  var cascadeHandler =
+      Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
 
   var handler =
       Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
---
+++ bin/models/noticia_model.dart
@@ -0,0 +1,3 @@
+class NoticiaModel {
+  final int id;
+}
