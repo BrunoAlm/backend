930dbd71fc798ddb8faa09220e39be33440d4c4e
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2023-01-22 14:09:35 -0300

    att

--- bin/apis/blog_api.dart
+++ bin/apis/blog_api.dart
@@ -1,28 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../models/noticia_model.dart';
+import '../services/generic_service.dart';
+
 class BlogApi {
+  final GenericService<NoticiaModel> _service;
+  BlogApi(this._service);
+
   Handler get handler {
     Router router = Router();
 
     // listagem
     router.get('/blog/noticias', (Request req) {
-      return Response.ok('Choveu hoje, alegria');
+      List<NoticiaModel> noticias = _service.findAll();
+      List<Map> noticiasMap = noticias.map((e) => e.toJson()).toList();
+
+      return Response.ok(jsonEncode(noticiasMap));
     });
 
     // nova noticia
-    router.post('/blog/noticias', (Request req) {
-      return Response.ok('Choveu hoje, alegria');
+    router.post('/blog/noticias', (Request req) async {
+      var body = await req.readAsString();
+      _service.save(NoticiaModel.fromJson(jsonDecode(body)));
+      return Response(201);
     });
 
     // /blog/noticias?id=1  // update
     router.put('/blog/noticias', (Request req) {
+      // _service.save();
       String? id = req.url.queryParameters['id'];
       return Response.ok('Choveu hoje, alegria');
     });
 
     // /blog/noticias?id=1  // delete
     router.delete('/blog/noticias', (Request req) {
+      // _service.delete(1);
       String? id = req.url.queryParameters['id'];
       return Response.ok('Choveu hoje, alegria');
     });
--- bin/models/noticia_model.dart
+++ bin/models/noticia_model.dart
@@ -1,3 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class NoticiaModel {
   final int id;
+  final String titulo;
+  final String descricao;
+  final String imagem;
+  final DateTime dtPublicacao;
+  final DateTime? dtAtualizacao;
+
+  NoticiaModel(
+    this.id,
+    this.titulo,
+    this.descricao,
+    this.imagem,
+    this.dtPublicacao,
+    this.dtAtualizacao,
+  );
+
+  factory NoticiaModel.fromJson(Map map) {
+    return NoticiaModel(
+      map['id'] ?? '',
+      map['titulo'],
+      map['descricao'],
+      map['imagem'],
+      DateTime.fromMicrosecondsSinceEpoch(map['dtPublicacao']),
+      map['dtAtualizacao'] != null
+          ? DateTime.fromMicrosecondsSinceEpoch(map['dtAtualizacao'])
+          : null,
+    );
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'titulo': titulo,
+      'descricao': descricao,
+      'imagem': imagem,
+      // 'dtPublicacao': dtPublicacao,
+      // 'dtAtualizacao': dtAtualizacao,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'NoticiaModel(id: $id, titulo: $titulo, descricao: $descricao, imagem: $imagem, dtPublicacao: $dtPublicacao, dtAtualizacao: $dtAtualizacao)';
+  }
 }
